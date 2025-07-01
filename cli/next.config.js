22656cc3a02853b8be075455b5c1ca0393923e55
nehaaaa8 <n47845149@gmail.com>
2025-07-01 16:57:02 +0530

    update

--- cli/next.config.js
+++ cli/next.config.js
@@ -28,7 +28,9 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        destination: `${
+          process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000"
+        }/api/:path*`,
       },
     ];
   },
